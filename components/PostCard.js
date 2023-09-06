cf97db386156e6e47bdb5a86bd92f256dc88ce36
Nathan Nguyen <nhanngyn15@gmail.com>
2023-09-06 12:59:40 -0400

    Allow photos in comment and add set exchange feature

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,6 +5,7 @@ import ClickOutHandler from 'react-clickout-handler';
 import Link from "next/link";
 import {
   CameraIcon,
+  XIcon,
   PhotographIcon,
 } from "@heroicons/react/solid";
 import {
@@ -15,13 +16,14 @@ import {
   TrashIcon,
   ExclamationIcon,
   PencilIcon,
+  ShoppingBagIcon,
 } from "@heroicons/react/outline";
 import ReactTimeAgo from 'react-time-ago';
 import { UserContext } from '@/contexts/UserContext';
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import PhotoModal from './PhotoModal';
 
-function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
+function PostCard({id, content, created_at, photos, sold, profiles:authorProfile}) {
   // State variables
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const {profile:myProfile} = useContext(UserContext);
@@ -31,10 +33,14 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
   const [comments, setComments] = useState([]);
   const [isSaved,setIsSaved] = useState(false);
   const [showReportPopup, setShowReportPopup] = useState(false);
+  const [showDeleteCommentPopup, setShowDeleteCommentPopup] = useState(false);
   const [showSavePopup, setShowSavePopup] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(0);
-
+  const [isUploading, setIsUploading] = useState();
+  const [uploads, setUploads] = useState([]);
+  const [isPosted, setIsPosted] = useState(false);
+  const session = useSession();
 // Fetch likes, comments, and check if the post is saved on component mount or when myProfile.id changes
   useEffect(() => {
     fetchLikes();
@@ -58,6 +64,7 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
       })
   }
 
+
   // Handles the submission of a report for the post
   function handleReportSubmit(e) {
     e.preventDefault();
@@ -66,6 +73,15 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
     // Hide the popup after a few seconds
     setTimeout(() => {
       setShowReportPopup(false);
+    }, 3000);
+  }
+
+  function handleDeleteComment() {
+    setShowDeleteCommentPopup(true);
+
+    // Hide the popup after a few seconds
+    setTimeout(() => {
+      setShowDeleteCommentPopup(false);
     }, 3000);
   }
 
@@ -87,6 +103,7 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
       .then(result => setLikes(result.data));
   }
 
+  
   // useEffect(() => {
   //   const channel = supabase
   //   .channel('likes')
@@ -111,6 +128,7 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
     supabase.from('posts')
       .select('*, profiles(*)')
       .eq('parent', id)
+      .order('created_at', {ascending: false})
       .then(result => setComments(result.data));
   }
 
@@ -197,22 +215,49 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
         });
     }
   }
+  // Function to toggle the 'sold' column in the 'posts' table
+  const toggleSoldStatus = async (e) => {
+    e.preventDefault()
+    
+        const { data, error } = await supabase
+          .from('posts')
+          .update({ sold: !sold }) 
+          .eq('id', id)
+          .select()
+          
+
   
-
-
+        if (error) {
+          console.error('Post sold error:', postError);
+          console.error('Sold request error:', error);
+        } 
+        if(data) {
+          console.log('Post set sold successfully:', sold);
+          // Perform any additional actions after successful post deletion
+          // window.location.reload();
+        }
+      }
+    
   // Submits a comment for the post
   function postComment(ev) {
     ev.preventDefault();
+    if (!commentText.trim()) {
+      // If commentText is empty or contains only spaces, do not proceed with the insert.
+      return;
+    }
     supabase.from('posts')
       .insert({
         content:commentText,
         author:myProfile.id,
+        photos: uploads,
         parent:id,
       })
       .then(result => {
         console.log(result);
         fetchComments();
         setCommentText('');
+        setUploads([]);
+        setIsPosted(true);
       })
   }
 
@@ -311,7 +356,72 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
     }
     window.location.reload();
   };
+
   
+    const [visibleComments, setVisibleComments] = useState(5);
+    
+    const loadMoreComments = () => {
+      setVisibleComments(prevVisibleComments => prevVisibleComments + 5);
+    };
+
+    const deleteComment = async (commentId) => {
+      const { data: postData, error: postError } = await supabase
+        .from('posts')
+        .delete()
+        .eq('id', commentId); // Use 'id' to target the specific comment
+    
+      if (postError) {
+        console.error('Comment delete error:', postError);
+      } else {
+        console.log('Comment deleted successfully:', postData);
+      }
+    };
+    
+    async function addPhotos(ev) {
+      const files = ev.target.files;
+      if (files.length > 0) {
+        if (files.length > 3) {
+          // Display an error message when the limit is exceeded
+          alert("You can only upload a maximum of 3 photos at a time.");
+          return;
+        }
+    
+        setIsUploading(true);
+    
+        for (const file of files) {
+          if (uploads.length >= 3) {
+            // If the limit is reached during the loop, break out of it
+            alert("You can only upload a maximum of 3 photos at a time.");
+            break;
+          }
+    
+          const newName = Date.now() + file.name;
+          const result = await supabase
+            .storage
+            .from('photos')
+            .upload(newName, file);
+          if (result.data) {
+            const url =
+              process.env.NEXT_PUBLIC_SUPABASE_URL +
+              '/storage/v1/object/public/photos/' +
+              result.data.path;
+            setUploads((prevUploads) => [...prevUploads, url]);
+          } else {
+            console.log(result);
+          }
+        }
+        setIsUploading(false);
+      }
+    }
+    
+    
+  
+    function removePhoto(index) {
+      setUploads(prevUploads => prevUploads.filter((_, i) => i !== index));
+    }
+
+    
+
   return (
     <Card>
       {/* Post header */}
@@ -329,7 +439,7 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
             <span className="font-semibold hover: animate-underline2 hover:scale-105 transition-all cursor-pointer">
               {authorProfile.name || `User ${authorProfile?.id}`}{' '}
             </span>
-            </Link> posted a <a className="text-red-500 font-semibold">listing</a>
+            </Link> {sold ? 'sold a' : 'posted a'} <a className="text-red-500 font-semibold">listing</a>
           </p>
           <p className="text-gray-400 text-sm font-semibold">
             <ReactTimeAgo date={(new Date(created_at)).getTime()} />
@@ -367,13 +477,14 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
                   </button>
                   {/* Edit button */}
                   {/* <a href="" className='group flex gap-2 py-2 my-2 hover:bg-red-500 hover:text-white -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'><BellIcon className="h-6 text-gray-800 group-hover:text-white" />Turn Notifications</a> */}
-                  {/* {myProfile?.id && myProfile.id === authorProfile.id && (
-                    <button onClick={editPost} className='w-full -my-2'>
-                      <span className='group flex gap-2 py-2 my-2 hover:bg-red-500 hover:text-white -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'>
-                        <PencilIcon className="h-6 text-gray-800 group-hover:text-white" />Edit Post
+                  {myProfile?.id && myProfile.id === authorProfile.id && (
+                    <button onClick={toggleSoldStatus} className='w-full -my-2'>
+                      <span className='group flex gap-2 py-2 my-2 hover:bg-red-500 hover:text-white dark:text-lightBG -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'>
+                        <ShoppingBagIcon className="h-6 text-gray-800 dark:text-lightBG group-hover:text-white" />
+                        {sold ? 'Undo Exchange' : 'Exchange Done'}
                       </span>
                     </button>
-                  )} */}
+                  )}
                   {/* Delete button */} 
                   {myProfile?.id && myProfile.id === authorProfile.id && (
                     <button onClick={deletePost} className='w-full -my-2'>
@@ -397,6 +508,11 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
               {showReportPopup && (
                 <div className="fixed bottom-5 right-5 bg-red-500 p-2 rounded-md shadow-md">
                   <p className="text-sm text-white">Post reported. Thank you for your feedback!</p>
+                </div>
+              )}
+              {showDeleteCommentPopup && (
+                <div className="fixed bottom-5 right-5 bg-red-500 p-2 rounded-md shadow-md">
+                  <p className="text-sm text-white">Comment Deleted. May take some time to reflect changes.</p>
                 </div>
               )}
               {showSavePopup && (
@@ -486,20 +602,57 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
         </div>
         {/* Comment input */}
         <div className='grow rounded-full h-12 dark:border-2 border-lightBorder dark:border-customBlack2 relative'>
-          <form onSubmit={postComment}>
-            <input
-              value={commentText}
-              onChange={ev => setCommentText(ev.target.value)}
-              className='dark:text-white bg-lightBG block w-full p-3 px-4 overflow-hidden h-12 rounded-full dark:bg-customBlack2 dark:placeholder-lightBorder' onFocus={(e) => e.target.placeholder = ""}
-              onBlur={(e) => e.target.placeholder = 'Leave a comment'} 
-              placeholder='Leave a comment'/>
-              
+          <form onSubmit={postComment} className='flex items-center'>
+            <div className="relative flex-grow">
+              <input
+                value={commentText}
+                onChange={ev => setCommentText(ev.target.value)}
+                className={`dark:text-white bg-lightBG block w-full p-3 px-4 overflow-hidden h-12 rounded-full dark:bg-customBlack2 dark:placeholder-lightBorder${
+                  sold ? 'opacity-50 pointer-events-none' : ''
+                }`}                
+                onFocus={(e) => e.target.placeholder = ""}
+                onBlur={(e) => e.target.placeholder = sold ? 'This exchange is completed' : 'Leave a comment'}
+                placeholder={sold ? 'This exchange is completed!' : 'Leave a comment'}
+                disabled={sold} // Disable input if isSold is true
+              />
+              <label className='flex gap-1 -mt-14 hover:scale-110 cursor-pointer absolute right-0 top-0'>
+                <input type="file" className='hidden' multiple onChange={addPhotos} />
+                <CameraIcon className={`h-7 ${sold? 'text-gray-300 hidden' : 'text-red-500 dark:text-gray-300'} mr-3 mt-16`} />
+              </label>
+            </div>
           </form>
         </div>
+        </div>
+    {/* Photo preview */}
+    {uploads.length > 0 && (
+      <div className="flex mt-2 gap-2">
+        {uploads.map((upload, index) => (
+          <div key={index} className="relative">
+            <img src={upload} alt="" className="w-auto h-16 rounded-md" />
+            <button
+              onClick={() => removePhoto(index)}
+              className="absolute top-0 right-0 p-1 bg-white rounded-full text-gray-500 hover:bg-gray-200 transition"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414L11.414 11l5.293 5.293a1 1 0 11-1.414 1.414L10 12.414l-5.293 5.293a1 1 0 01-1.414-1.414L8.586 11 3.293 5.707a1 1 0 111.414-1.414L10 9.586l5.293-5.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        ))}
       </div>
+    )}
       <div>
         {/* Comment section */}
-        {comments.length > 0 && comments.map(comment => (
+        {comments.slice(0, visibleComments).map(comment => (
           <div key={comment.id} className="mt-2 flex gap-2 items-center">
             <Link href={'/profile/' + comment.profiles.id}>
               <span className='cursor-pointer'>
@@ -507,7 +660,7 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
               </span>
             </Link>
             <div className="bg-lightBG py-2 px-4 rounded-3xl dark:bg-customBlack2">
-              <div>
+              <div className='flex items-center'>
                 <Link href={'/profile/' + comment.profiles.id}>
                   <span className="hover:underline font-semibold mr-1 dark:text-lightBG">
                     {comment.profiles.name}
@@ -516,11 +669,44 @@ function PostCard({id, content, created_at, photos, profiles:authorProfile}) {
                 <span className="text-sm text-gray-400">
                   <ReactTimeAgo timeStyle={'twitter'} date={(new Date(comment.created_at)).getTime()} />
                 </span>
+                {session?.user?.id == comment.profiles.id && (
+                  <button
+                  onClick={() => {
+                    deleteComment(comment.id);
+                    handleDeleteComment();
+                  }}
+                  className="relative group"
+                >
+                  <XIcon className='h-4 text-red-500 dark:text-gray-300 ml-4' />
+                  <span className="opacity-0 dark:bg-black bg-taupe text-white text-center text-xs rounded-lg py-2 absolute z-10 w-32 left-1/2 transform -translate-x-1/2 top-full mt-2 pointer-events-none transition-opacity duration-300 group-hover:opacity-100">
+                    Delete the comment
+                  </span>
+                </button>
+                
+                )}
+                
               </div>
               <p className="text-sm dark:text-lightBG">{comment.content}</p>
+              {comment.photos && comment.photos.length > 0 && (
+                  <div className='flex gap-2 mt-1'>
+                    {comment.photos.slice(0, 3).map((photo, index) => (
+                      <div key={photo} className='rounded-md overflow-hidden'>
+                        <img src={photo} className='mx-auto w-full h-full object-cover' alt="" />
+                      </div>
+                    ))}
+                  </div>
+              )}
             </div>
           </div>
         ))}
+         {comments.length > visibleComments && (
+        <button
+          onClick={loadMoreComments}
+          className="bg-red-400 text-white py-2 px-4 rounded-full mt-4 hover:bg-red-500"
+        >
+          Load More
+        </button>
+      )}
       </div>
     </Card>
   )
