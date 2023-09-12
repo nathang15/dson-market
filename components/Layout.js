ec1b6b0fcc4beb094d377a2f76e59159f8873689
Nathan Nguyen <nhanngyn15@gmail.com>
2023-09-11 20:11:46 -0400

    Update guideline and add photo limit to post form

--- components/Layout.js
+++ components/Layout.js
@@ -16,7 +16,7 @@ function Layout({children}) {
         <div className="bg-white border-2 border-lightBorder dark:bg-customBlack dark:border-customBlack2 -mt-1">
           <Sidebar />
         </div>
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide dark:bg-darkBG">
+        <div className="flex-grow h-screen pb-24 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide dark:bg-darkBG">
             {children}
         </div>
       </main>
