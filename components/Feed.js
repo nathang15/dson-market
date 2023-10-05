7b5d7d3d028bfa31e5c40685e46f37b60b2ffe4d
Nathan Nguyen <nhanngyn15@gmail.com>
2023-10-05 00:22:00 -0400

    add dark mode for sign in page and add sorting by time function and add otp button at sign up

--- components/Feed.js
+++ components/Feed.js
@@ -4,6 +4,9 @@ import PostCard from './PostCard';
 import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react';
 import {useEffect, useState} from 'react';
 import {UserContext} from '@/contexts/UserContext';
+import {
+  MenuAlt2Icon,
+} from '@heroicons/react/solid';
 
 /**
  * A React component for displaying the feed.
@@ -19,6 +22,8 @@ function Feed() {
 
   // Initialize state variable for the user's profile
   const [profile, setProfile] = useState({});
+
+  const [sortBy, setSortBy] = useState('desc');
 
   // Subscribe to changes in the 'posts' channel when the component mounts, enables real-time posts
   useEffect(() => {
@@ -42,7 +47,7 @@ function Feed() {
   // Fetch posts when the component mounts
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [sortBy]);
 
   // Fetch posts and user profile when the session or user ID changes
   useEffect(() => {
@@ -64,21 +69,42 @@ function Feed() {
    * Fetches the posts from the Supabase database.
    */
   function fetchPosts() {
-    supabase.from('posts').select('id, content, created_at, photos, sold, profiles(id, avatar, name)').is('parent', null).order('created_at', {ascending: false}).then((result) => {
-      setPosts(result.data);
-    });
+    supabase
+        .from('posts')
+        .select('id, content, created_at, photos, sold, profiles(id, avatar, name)')
+        .is('parent', null)
+        .order('created_at', {ascending: sortBy === 'asc'})
+        .then((result) => {
+          setPosts(result.data);
+        });
   }
+
+  const toggleSortOrder = () => {
+    // Toggle the sorting order when the button is clicked
+    setSortBy(sortBy === 'asc' ? 'desc' : 'asc');
+  };
 
   return (
     <div className="flex-grow h-screen pb-44 md:pt-6 pt-0 md:mr-4 mr-2 md:ml-0 ml-2 xl:mr-40 overflow-auto scrollbar-hide">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <UserContext.Provider value={{profile}}>
-          <PostFormCard onPost={fetchPosts}/>
-          {posts?.length > 0 ? (
+          <div>
+            <PostFormCard onPost={fetchPosts}/>
+            <button
+              onClick={toggleSortOrder}
+              className='bg-red-500 hover:scale-105 text-white px-2 py-2 rounded-md mb-3 -mt-2 dark:bg-customBlack dark:border-customBlack2 dark:border-2 transition-all'
+            >
+              <div className='flex items-center justify-start'>
+                <MenuAlt2Icon className="h-6 text-lightBG group-hover:text-white transition duration-300 ease-in-out transform" />
+                {sortBy === 'asc' ? 'Ascending' : 'Descending'}
+              </div>
+            </button>
+            {posts?.length > 0 ? (
             posts.map((post) => <PostCard key={post.id} {...post} />)
           ) : (
             <p className="dark:text-lightBG text-darkBG text-center md:text-2xl text-lg font-bold">Be the first one to post!</p>
           )}
+          </div>
         </UserContext.Provider>
       </div>
     </div>
