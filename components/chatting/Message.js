0d4e63d70e182da4e9afef3dae6ba26eacd55b3b
Nathan Nguyen <nhanngyn15@gmail.com>
2023-12-21 21:42:49 -0500

    functional chat

--- components/chatting/Message.js
+++ components/chatting/Message.js
@@ -1,7 +1,51 @@
+// import React from 'react';
+// import {UserContext} from '@/contexts/UserContext';
+// import {useContext, useEffect, useState} from 'react';
+// import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react';
+// import {UserContextProvider} from '@/contexts/UserContext';
+// import Link from 'next/link';
+// import Avatar from '../Avatar';
+// const Message = ({messages}) => {
+//   const session = useSession();
+//   const supabase = useSupabaseClient();
+//   const {profile} = useContext(UserContext);
+//   const isUser = profile?.id;
+
+//   // {messages.map((message) => (
+//   //   <Message key={message.id} message={content} />
+//   // ))}
+//   return (
+//     <div>
+//       {messages.map((message) => (
+//         <div className={`flex ${isUser ? 'justify-end' : 'justify-start'} items-center gap-2 mb-2 last:mb-0`} key={message.id}>
+//           <div
+//             className={`${isUser ? 'bg-red-400' : 'dark:bg-darkBG bg-lightBG'}
+//             ${isUser ? 'text-white' : 'dark:text-white text-darkBG'} p-2 mx-1 rounded-md max-w-xs`}
+//           >
+//             {message.text}
+//           </div>
+//           <div className='flex items-center'>
+//             {isUser && profile ? (
+//               <Link href={'/profile/' + profile.id}>
+//                 <span className='cursor-pointer'>
+//                   <Avatar url={profile.avatar} />
+//                 </span>
+//               </Link>
+//             ) : null}
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Message;
+
 import React from 'react';
 import {UserContext} from '@/contexts/UserContext';
 import {useContext, useEffect, useState} from 'react';
 import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react';
+import {useRouter} from 'next/router';
 import {UserContextProvider} from '@/contexts/UserContext';
 import Link from 'next/link';
 import Avatar from '../Avatar';
@@ -10,34 +54,57 @@ const Message = ({message}) => {
   const supabase = useSupabaseClient();
   const {profile} = useContext(UserContext);
   const isUser = message.sender === 'user';
+  const router = useRouter();
+  const userId = router.query.id;
+  const [otherUserProfile, setOtherUserProfile] = useState(null);
 
+  useEffect(() => {
+    const loadOtherUserProfile = async () => {
+      // Load the profile of the other user
+      const {data, error} = await supabase
+          .from('profiles')
+          .select('id, avatar')
+          .eq('id', userId)
+          .single();
+
+      if (error) {
+        console.error('Error loading other user profile:', error);
+      } else {
+        setOtherUserProfile(data);
+      }
+    };
+
+    loadOtherUserProfile();
+  }, [isUser, message.author]);
+  console.log(otherUserProfile);
 
   return (
     <div className={`flex ${isUser ? 'justify-end' : 'justify-start'} items-center gap-2 mb-2 last:mb-0`}>
-      {/* <div className='flex items-center'>
-        <Link href={'/profile/' + profile?.id}>
-          <span className='cursor-pointer'>
-            <Avatar url={profile?.avatar} />
-          </span>
-        </Link>
-      </div> */}
+      {!isUser && otherUserProfile && (
+        <div className='flex items-center'>
+          <Link href={'/profile/' + otherUserProfile?.id}>
+            <span className='cursor-pointer'>
+              <Avatar url={otherUserProfile?.avatar} />
+            </span>
+          </Link>
+        </div>
+      )}
       <div
-        className={`${isUser ? 'bg-red-400' : 'dark:bg-customBlack bg-lightBG'} 
+        className={`${isUser ? 'bg-red-400' : 'dark:bg-customBlack2 bg-lightBG'} 
         ${isUser ? 'text-white' : 'dark:text-white text-darkBG'} p-2 mx-1 rounded-md max-w-xs`}
       >
         {message.text}
       </div>
-      <div className='flex items-center'>
-        {isUser && profile ? (
+      {isUser && profile && (
+        <div className='flex items-center'>
           <Link href={'/profile/' + profile.id}>
             <span className='cursor-pointer'>
               <Avatar url={profile.avatar} />
             </span>
           </Link>
-        ) : null}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
-
 export default Message;
--- pages/chat.js
+++ pages/chat.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import PostCard from '@/components/PostCard';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useContext} from 'react';
 import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react';
 import {UserContextProvider} from '../contexts/UserContext';
 import Message from '@/components/chatting/Message';
 import MessageInput from '@/components/chatting/MessageInput';
 import {useRouter} from 'next/router';
+import Card from '@/components/Card';
+import {UserContext} from '@/contexts/UserContext';
+import Link from 'next/link';
+import Avatar from '@/components/Avatar';
+
 /**
  * Saved Post Page
  * @return {JSX.Element} The rendered Saved Post Page
@@ -15,41 +20,116 @@ function ChatPage() {
   const session = useSession();
   const supabase = useSupabaseClient();
   const router = useRouter();
-  const [profile, setProfile] = useState(null);
+  const {profile} = useContext(UserContext);
   const userId = router.query.id;
-  // TODO Should replace with calls to fetch from messages table
-  const [messages, setMessages] = useState([
-    {id: 1, text: 'Hello!', sender: 'user'},
-    {id: 2, text: 'Hi there!', sender: 'otherUser'},
-    // Add more messages as needed
-  ]);
+  const [messageText, setMessageText] = useState('');
+  const [messages, setMessages] = useState([]);
 
-  // TODO Replace with calls to post messages to messages table
-  const sendMessage = (text, sender) => {
-    const newMessage = {
-      id: messages.length + 1,
-      text,
-      sender,
-    };
-    setMessages([...messages, newMessage]);
+  const channel = supabase
+      .channel('messages')
+      .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'messages',
+          },
+          (payload) => loadMessages(),
+      )
+      .subscribe();
+
+  useEffect(() => {
+    // Load messages when the component mounts or userId changes
+    loadMessages();
+  }, [userId]);
+
+  const sendMessage = async () => {
+    try {
+      if (!messageText.trim()) {
+        return; // Handle case where message text is empty
+      }
+
+      const authorId = session.user.id;
+      const receiverId = userId;
+
+      // Insert the message into the 'messages' table
+      const {data, error} = await supabase
+          .from('messages')
+          .insert([
+            {content: messageText, author: authorId, receiver: receiverId},
+          ]);
+
+      if (error) {
+        throw error;
+      }
+
+      // Update local state with the new message
+      const newMessage = {
+        id: data[0].id,
+        text: messageText,
+        sender: 'user',
+      };
+
+      setMessages([...messages, newMessage]);
+    } catch (error) {
+      console.error(error);
+    }
+    // Clear the message text input after successful submission
+    setMessageText('');
   };
 
+  const loadMessages = async () => {
+    try {
+      const {data, error} = await supabase
+          .from('messages')
+          .select('id, content, author, receiver, created_at')
+          .in('author', [session.user.id, userId]) // Select messages from both users
+          .order('created_at', {ascending: true});
+
+      if (error) {
+        throw error;
+      }
+
+      // Map fetched messages to the format used in the local state
+      const formattedMessages = data.map((message) => ({
+        id: message.id,
+        text: message.content,
+        sender: message.author === session.user.id ? 'user' : 'otherUser',
+      }));
+
+      setMessages(formattedMessages);
+    } catch (error) {
+      console.error(error);
+      // Handle error accordingly
+    }
+  };
+
+
   return (
-    <Layout>
-      <UserContextProvider>
-        <h1 className='flex justify-center md:text-6xl text-4xl mb-6 text-darkBG dark:text-lightBG underline decoration-red-500 -my-3 font-bold decoration-8'>
-          {userId}
-        </h1>
-        <div className="rounded-md flex flex-col justify-end p-4 dark:bg-customBlack2 bg-gray-300 -my-3" style={{width: '77vw', height: '82vh'}}>
-          <div className="flex-1 mt-4">
-            {messages.map((message) => (
-              <Message key={message.id} message={message} />
-            ))}
-          </div>
-          <div><MessageInput sendMessage={sendMessage} /></div>
+    <Card>
+      <div className="flex-1 mt-4">
+        {messages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      <div className="mt-4">
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+            placeholder="Type your message..."
+            className="flex-1 p-2 border rounded-md mr-2 w-12 dark:text-white dark:bg-customBlack2 dark:border-darkBG"
+          />
+          <button
+            onClick={sendMessage}
+            className="bg-red-500 text-white p-2 rounded-md"
+          >
+            Send
+          </button>
         </div>
-      </UserContextProvider>
-    </Layout>
+      </div>
+    </Card>
   );
 }
 
