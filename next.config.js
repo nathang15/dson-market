634b719a9c6f16ec5e75bf64cdcd7fd5704c6368
Nathan Nguyen <nhanngyn15@gmail.com>
2024-02-25 22:46:22 -0500

    update dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
 
-module.exports = {
-  nextConfig,
-}
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
