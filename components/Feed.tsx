cb625e99c2f7b77f4892a37340526bf469294b8a
Nathan Nguyen <nhanngyn15@gmail.com>
2024-10-02 12:40:47 -0400

    feed hot fix

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,117 +1,107 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { UserContext } from '@/contexts/UserContext';
-import { MenuAlt2Icon } from '@heroicons/react/solid';
+import React from 'react';
 import PostFormCard from './PostFormCard';
 import PostCard from './PostCard';
-import { useInView } from 'react-intersection-observer';
+import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react';
+import {useEffect, useState} from 'react';
+import {UserContext} from '@/contexts/UserContext';
+import {
+  MenuAlt2Icon,
+} from '@heroicons/react/solid';
 
-const POSTS_PER_PAGE = 5;
-
+/**
+ * A React component for displaying the feed.
+ * @return {JSX.Element} - The rendered React element.
+ */
 function Feed() {
+  // Fetch the user's session
   const session = useSession();
+  // Initialize state variables for posts and the Supabase client
   const [posts, setPosts] = useState([]);
   const supabase = useSupabaseClient();
+  // Initialize state variable for the user's profile
   const [profile, setProfile] = useState({});
   const [sortBy, setSortBy] = useState('desc');
-  const [page, setPage] = useState(0);
-  const [hasMore, setHasMore] = useState(true);
-  const [ref, inView] = useInView({
-    threshold: 0,
-  });
 
-  const fetchPosts = useCallback(async () => {
-    const { data, error } = await supabase
-      .from('posts')
-      .select('id, content, created_at, photos, sold, profiles(id, avatar, name)')
-      .is('sold', false)
-      .is('parent', null)
-      .order('created_at', { ascending: sortBy === 'asc' })
-      .range(page * POSTS_PER_PAGE, (page + 1) * POSTS_PER_PAGE - 1);
-
-    if (error) {
-      console.error('Error fetching posts:', error);
-      return;
-    }
-
-    if (data.length < POSTS_PER_PAGE) {
-      setHasMore(false);
-    }
-
-    setPosts((prevPosts) => [...prevPosts, ...data]);
-    setPage((prevPage) => prevPage + 1);
-  }, [supabase, sortBy, page]);
-
+  // Subscribe to changes in the 'posts' channel when the component mounts, enables real-time posts
   useEffect(() => {
     const subscription = supabase
-      .channel('posts')
-      .on('postgres_changes', { event: '*', schema: 'public' }, () => {
-        setPosts([]);
-        setPage(0);
-        setHasMore(true);
-        fetchPosts();
-      })
-      .subscribe();
+        .channel('posts')
+        .on(
+            'postgres_changes',
+            {
+              event: '*',
+              schema: 'public',
+            },
+            (payload) => fetchPosts(),
+        )
+        .subscribe();
 
     return () => {
       supabase.removeChannel(subscription);
     };
-  }, [supabase, fetchPosts]);
+  }, []);
 
+  // Fetch posts when the component mounts
   useEffect(() => {
-    if (inView && hasMore) {
-      fetchPosts();
+    fetchPosts();
+  }, [sortBy]);
+
+  // Fetch posts and user profile when the session or user ID changes
+  useEffect(() => {
+    fetchPosts();
+    if (!session?.user?.id) {
+      return;
     }
-  }, [inView, hasMore, fetchPosts]);
+    supabase.from('profiles')
+        .select()
+        .eq('id', session.user.id)
+        .then((result) => {
+          if (result.data.length) {
+            setProfile(result.data[0]);
+          }
+        });
+  }, [session?.user?.id]);
 
-  useEffect(() => {
-    if (!session?.user?.id) return;
-
+  /**
+   * Fetches the posts from the Supabase database.
+   */
+  function fetchPosts() {
     supabase
-      .from('profiles')
-      .select()
-      .eq('id', session.user.id)
-      .then((result) => {
-        if (result.data.length) {
-          setProfile(result.data[0]);
-        }
-      });
-  }, [session?.user?.id, supabase]);
+        .from('posts')
+        .select('id, content, created_at, photos, sold, profiles(id, avatar, name)')
+        .is('sold', false)
+        .is('parent', null)
+        .order('created_at', {ascending: sortBy === 'asc'})
+        .then((result) => {
+          setPosts(result.data);
+        });
+  }
 
   const toggleSortOrder = () => {
-    setSortBy((prevSort) => (prevSort === 'asc' ? 'desc' : 'asc'));
-    setPosts([]);
-    setPage(0);
-    setHasMore(true);
+    // Toggle the sorting order when the button is clicked
+    setSortBy(sortBy === 'asc' ? 'desc' : 'asc');
   };
 
   return (
     <div className="flex-grow h-screen pb-44 md:pt-6 pt-0 overflow-auto">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
-        <UserContext.Provider value={{ profile }}>
+        <UserContext.Provider value={{profile}}>
           <div>
-            <PostFormCard onPost={fetchPosts} />
+            <PostFormCard onPost={fetchPosts}/>
             <button
               onClick={toggleSortOrder}
-              className="bg-red-500 hover:scale-105 text-white px-2 py-2 rounded-md mb-3 -mt-2 dark:bg-customBlack dark:border-customBlack2 dark:border-2 transition-all"
+              className='bg-red-500 hover:scale-105 text-white px-2 py-2 rounded-md mb-3 -mt-2 dark:bg-customBlack dark:border-customBlack2 dark:border-2 transition-all'
             >
-              <div className="flex items-center justify-start">
+              <div className='flex items-center justify-start'>
                 <MenuAlt2Icon className="h-6 text-lightBG group-hover:text-white transition duration-300 ease-in-out transform" />
                 {sortBy === 'asc' ? 'Ascending' : 'Descending'}
               </div>
             </button>
-            {posts.length > 0 ? (
-              posts.map((post) => <PostCard key={post.id} {...post} />)
-            ) : (
-              <p className="dark:text-lightBG text-darkBG text-center md:text-2xl text-lg font-bold">
-                Be the first one to post!
-              </p>
-            )}
-            {hasMore && (
-              <div ref={ref} className="h-10 flex items-center justify-center">
-                <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-red-500"></div>
-              </div>
-            )}
+            {posts?.length > 0 ? (
+            posts.map((post) => <PostCard key={post.id} {...post} />)
+          ) : (
+            <p className="dark:text-lightBG text-darkBG text-center md:text-2xl text-lg font-bold">Be the first one to post!</p>
+          )}
           </div>
         </UserContext.Provider>
       </div>
